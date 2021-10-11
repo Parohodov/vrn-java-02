a8cf2ba72dc8769d24a8e16413072cb445bc0461
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-10-11 10:46:32 +0300

    Native approach

--- src/main/java/ru/dataart/academy/java/LongestSubstring.java
+++ src/main/java/ru/dataart/academy/java/LongestSubstring.java
@@ -15,17 +15,19 @@ public class LongestSubstring {
         for (int s = 0; s < checkString.length(); s++) {
             StringBuilder sb = new StringBuilder();
             String subString = "";
+            String currChar = "";
 
-            int e = s;
-            for (; e < checkString.length(); e++) {
-                String currChar = checkString.substring(e,e+1);
+            for (int e = s; e < checkString.length(); e++) {
+                currChar = checkString.substring(e,e+1);
                 if (subString.contains(currChar)) {
                     break;
                 }
                 subString = sb.append(checkString.charAt(e)).toString();
             }
             subLength = Math.max(subLength,sb.length());
+            s += subString.indexOf(currChar);
         }
+
         return subLength;
     }
 }
