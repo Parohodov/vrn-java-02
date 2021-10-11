448e6d37f0b75e5a1ed7999382952a480563fb52
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-10-11 12:36:01 +0300

    A little bit smarter approach

--- src/main/java/ru/dataart/academy/java/TwoSums.java
+++ src/main/java/ru/dataart/academy/java/TwoSums.java
@@ -1,5 +1,7 @@
 package ru.dataart.academy.java;
 
+import java.util.Arrays;
+
 public class TwoSums {
     /**
      * @param nums   - sorted in ascending order array of integer values >=0
@@ -13,13 +15,28 @@ public class TwoSums {
     public int[] getTwoSum(int[] nums, int target) {
         // Task implementation
 
+        if (nums.length == 0) {
+            return new int[]{};
+        }
+
+        if (nums[0] > target) {
+            return new int[]{};
+        }
+
         int first = 0;
         int second = 0;
         boolean numsExist = false;
+        int maxValue = nums[nums.length-1];
 
         label:
         for (int i = 0; i < nums.length; i++) {
             first = nums[i];
+
+            // There is no point to check if a current number meets the condition with any number from an array
+            // if it doesn't with a maximum value (because array is sorted)
+            if (first < target - maxValue) {
+                continue;
+            }
 
             for (int j = i + 1; j < nums.length; j++) {
                 second = nums[j];
@@ -28,7 +45,12 @@ public class TwoSums {
                     break label;
                 }
             }
+
+            // In case of multiple choices
+            // If (prevNumber + maxValue == target) than (currNumber + maxValue > target) for sure
+//            maxValue = nums[length-1];
         }
+
         if (numsExist) {
             return new int[]{first, second};
         } else {
